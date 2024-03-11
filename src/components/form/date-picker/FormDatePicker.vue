29847ce0eb152c5184dc0b8905225628852091f8
Wouter Laermans <wouter.laermans@appwise.be>
2024-03-11 09:16:13 +0100

    feat: add `DatePicker` component

--- src/components/form/date-picker/FormDatePicker.vue
+++ src/components/form/date-picker/FormDatePicker.vue
@@ -1,34 +1,33 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 
+import FormGroup from '../group/FormGroup.vue'
+import FormLabel from '../label/FormLabel.vue'
 import AppDatePicker from '@/components/app/date-picker/AppDatePicker.vue'
 import { useIsFormInvalid } from '@/composables/form/form.composable'
 import type { FormErrors } from '@/types/form/formErrors.type'
 import { generateUuid } from '@/utils/uuid/generateUuid.util'
 
-import FormGroup from '../group/FormGroup.vue'
-import FormLabel from '../label/FormLabel.vue'
-
 const props = withDefaults(
-	defineProps<{
-		minDate?: Date | null
-		maxDate?: Date | null
-		isDisabled?: boolean
-		isRequired?: boolean
-		errors: FormErrors
-		isTouched: boolean
-		label: string
-	}>(),
-	{
-		minDate: null,
-		maxDate: null,
-		isDisabled: false,
-		isRequired: false,
-	}
+  defineProps<{
+    minDate?: Date | null
+    maxDate?: Date | null
+    isDisabled?: boolean
+    isRequired?: boolean
+    errors: FormErrors
+    isTouched: boolean
+    label: string
+  }>(),
+  {
+    minDate: null,
+    maxDate: null,
+    isDisabled: false,
+    isRequired: false,
+  },
 )
 
 const model = defineModel<Date | null>({
-	required: true,
+  required: true,
 })
 
 const id = `form-date-picker-${generateUuid()}`
@@ -40,22 +39,22 @@ const isFormInvalid = useIsFormInvalid(errors, isTouched)
 </script>
 
 <template>
-	<FormGroup>
-		<FormLabel
-			v-if="label"
-			:id="id"
-			:is-disabled="props.isDisabled"
-			:is-invalid="isFormInvalid"
-			:is-required="props.isRequired"
-			:label="props.label"
-		/>
+  <FormGroup>
+    <FormLabel
+      v-if="label"
+      :id="id"
+      :is-disabled="props.isDisabled"
+      :is-invalid="isFormInvalid"
+      :is-required="props.isRequired"
+      :label="props.label"
+    />
 
-		<AppDatePicker
-			:id="id"
-			v-model="model"
-			:is-invalid="isFormInvalid"
-			:max-date="maxDate"
-			:min-date="minDate"
-		/>
-	</FormGroup>
+    <AppDatePicker
+      :id="id"
+      v-model="model"
+      :is-invalid="isFormInvalid"
+      :max-date="maxDate"
+      :min-date="minDate"
+    />
+  </FormGroup>
 </template>
